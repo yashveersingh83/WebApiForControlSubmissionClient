9711c4e9ae5909b0c2a2598ff60a18870ab02798
yashveersingh83 <yashveersingh83@gmail.com>
2016-12-07 22:26:52 +0100

    Backend files

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            //   config.SuppressDefaultHostAuthentication();
+            //    config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            //     var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            //      json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            config.MessageHandlers.Add(new PreflightRequestsHandler());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+
+    public class PreflightRequestsHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Headers.Contains("Origin") && request.Method.Method == "OPTIONS")
+            {
+                var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
+                response.Headers.Add("Access-Control-Allow-Origin", "*");
+                response.Headers.Add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+                response.Headers.Add("Access-Control-Allow-Methods", "*");
+                var tsc = new TaskCompletionSource<HttpResponseMessage>();
+                tsc.SetResult(response);
+                return tsc.Task;
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
